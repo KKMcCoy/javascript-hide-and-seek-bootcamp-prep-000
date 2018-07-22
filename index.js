1820bccd20de8cf117b84c1c8d7636eee69e4439
karen S <buzz.kyt@gmail.com>
2018-07-22 19:45:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function nestedTarget() {
 function increaseRankBy(n) {
   var rankedNum = document.querySelectorAll('.ranked-list>li');
   for (let i = 0; i < document.querySelectorAll('.ranked-list>li'); i++) {
-   return rankedNum[i].innerHTML += n;
+   return parseInt(rankedNum[i].innerHTML) += n;
   }
 }

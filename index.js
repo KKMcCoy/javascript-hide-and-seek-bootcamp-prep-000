c709fdc3bcfe82378a2f872e44b7faad757dedc6
karen S <buzz.kyt@gmail.com>
2018-07-22 19:22:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function getFirstSelector(selector) {
 
 function nestedTarget(nested, target) {
   var byID = document.getElementsById(nested);
-  return byID.getElementsByClassName(target);
+  return byID.querySelectorAll(target);
 }

762d233005d496a53ceb8f1780a29f75264d1d9e
karen S <buzz.kyt@gmail.com>
2018-07-22 19:26:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function getFirstSelector(selector) {
 
 function nestedTarget() {
   var byID = document.getElementById('nested');
-  return byID.querySelectorAll('target');
+  return byID.querySelectorAlller('target');
 }

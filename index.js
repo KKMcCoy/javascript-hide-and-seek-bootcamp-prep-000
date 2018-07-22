69f97eef7941ade46a0854fb46dd4b7c96649550
karen S <buzz.kyt@gmail.com>
2018-07-22 19:45:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  var rankedNum = document.querySelectorAll('.ranked-list');
-  for (let i = 0; i < document.querySelectorAll('.ranked-list'); i++) {
+  var rankedNum = document.querySelectorAll('.ranked-list li');
+  for (let i = 0; i < document.querySelectorAll('.ranked-list li'); i++) {
    return rankedNum[i].innerHTML += n;
   }
 }

dff033ee11bd35547de2a51d9b8dee992fdd81bd
karen S <buzz.kyt@gmail.com>
2018-07-22 21:11:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function increaseRankBy(n) {
   var list = document.querySelectorAll('.ranked-list');
   for (let i = 0; i < list.length ; i++) {
  
-  var children = list[i];
+  var children = list.children;
      for (let i=0; i < children.length; i++ ){
       children[i].innerHTML = parseInt(children[i].innerHTML) + n;
   } return children;

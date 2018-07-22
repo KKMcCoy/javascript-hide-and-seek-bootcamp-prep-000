9edb760cf86350a2a886d7a70bddbaa1ea7f280b
karen S <buzz.kyt@gmail.com>
2018-07-22 21:28:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  var list = document.querySelectorAll('.ranked-list');
+  for (let i = 0; i < list.length ; i++) {
+ 
+  var children = list[i].children;
+     for (let i=0; i < children.length; i++ ){
+      children[i].innerHTML = parseInt(children[i].innerHTML) + n;
+  }
+  } return list;
+}
+
+function deepestChild() {
+  return document.querySelector('div#grand-node div div div div');
+}
